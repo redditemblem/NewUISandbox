e629115874a6735cf46b1960f3609bb02a011ea5
Iron Pegasus <kayleebanana@gmail.com>
2026-07-08 23:19:49 -0500

    Updating readme, map segment progress

--- README.md
+++ README.md
@@ -1,59 +1,18 @@
-# RedditEmblemMapsUI
+# Reddit Emblem Maps UI
+Reddit Emblem Maps is web-based display that is custom built for the Reddit Emblem play-by-post community on Discord. Maps aims to be highly-configurable and flexible, to suit a wide range of system designs and customized mechanics.
 
-This project was generated using [Angular CLI](https://github.com/angular/angular-cli) version 21.2.8.
+Maps' UI is an Angular web application responsible for consuming and displaying JSON team data returned from the [Reddit Emblem Maps API](https://github.com/redditemblem/RedditEmblemAPI). If you are a Reddit Emblem player interested in integrating your team with Maps, [user documentation can be found in Google Docs](https://docs.google.com/document/d/150h2pxQbrd3uZOKM_r3iniCRBPtnfIJbSTnWlm-pRUM/edit?usp=sharing).
 
-## Development server
-
-To start a local development server, run:
-
-```bash
-ng serve
-```
-
-Once the server is running, open your browser and navigate to `http://localhost:4200/`. The application will automatically reload whenever you modify any of the source files.
-
-## Code scaffolding
-
-Angular CLI includes powerful code scaffolding tools. To generate a new component, run:
-
-```bash
-ng generate component component-name
-```
-
-For a complete list of available schematics (such as `components`, `directives`, or `pipes`), run:
-
-```bash
-ng generate --help
-```
-
-## Building
-
-To build the project run:
-
-```bash
-ng build
-```
-
-This will compile your project and store the build artifacts in the `dist/` directory. By default, the production build optimizes your application for performance and speed.
-
-## Running unit tests
-
-To execute unit tests with the [Vitest](https://vitest.dev/) test runner, use the following command:
-
-```bash
-ng test
-```
-
-## Running end-to-end tests
-
-For end-to-end (e2e) testing, run:
-
-```bash
-ng e2e
-```
-
-Angular CLI does not come with an end-to-end testing framework by default. You can choose one that suits your needs.
-
-## Additional Resources
-
-For more information on using the Angular CLI, including detailed command references, visit the [Angular CLI Overview and Command Reference](https://angular.dev/tools/cli) page.
+<p align="center">
+    <a href="https://code.visualstudio.com/"><img src="https://custom-icon-badges.demolab.com/badge/Visual%20Studio%20Code-0078d7.svg?logo=visualstudiocode&logoColor=white" alt="Visual Studio Code"></a>
+    <a href="https://pages.github.com/"><img src="https://img.shields.io/badge/GitHub%20Pages-121013?logo=github&logoColor=white" alt="Github Pages"></a>
+    <a href="https://www.npmjs.com/"><img src="https://img.shields.io/badge/npm-CB3837?logo=npm&logoColor=fff" alt="NPM"></a>
+    <a href="https://insomnia.rest/"><img src="https://img.shields.io/badge/Insomnia-4000BF?logo=insomnia&logoColor=white" alt="Insomnia"></a>
+    
+</p>
+<p align="center">
+    <a href="https://www.typescriptlang.org/"><img src="https://img.shields.io/badge/TypeScript-3178C6?logo=typescript&logoColor=fff" alt="Typescript"></a>
+    <a href="https://nodejs.org/en"><img src="https://img.shields.io/badge/Node.js-6DA55F?logo=node.js&logoColor=white" alt="Node.js"></a>
+    <a href="https://angular.dev/"><img src="https://img.shields.io/badge/Angular-%23DD0031.svg?logo=angular&logoColor=white" alt="Angular"></a>
+    <a href="https://sass-lang.com/"><img src="https://img.shields.io/badge/Sass-C69?logo=sass&logoColor=fff" alt="SaSS"></a>
+</p>
--- src/app/components/views/map-segment/map-segment.ts
+++ src/app/components/views/map-segment/map-segment.ts
@@ -1,6 +1,7 @@
-import { Component, input } from '@angular/core';
+import { Component, inject, input } from '@angular/core';
 import { MapSegment as IMapSegment } from '../../../interfaces/map/map-segment';
-import { Application, Assets, Container, Sprite } from 'pixi.js';
+import { Application, Assets, Container, Sprite, Texture } from 'pixi.js';
+import { MatSnackBar, MatSnackBarRef, TextOnlySnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'map-segment',
@@ -11,10 +12,17 @@ import { Application, Assets, Container, Sprite } from 'pixi.js';
 export class MapSegment {
   segment = input.required<IMapSegment>();
 
+  private snackBar : MatSnackBar;
+  private snackBarMessageQueue : string[];
+  private currentSnackBar : MatSnackBarRef<TextOnlySnackBar> | undefined;
+
   private pixiApp : Application;
 
   constructor() {
+    this.snackBar = inject(MatSnackBar);
     this.pixiApp = new Application();
+
+    this.snackBarMessageQueue = [];
   }
 
   async ngOnInit() {
@@ -25,7 +33,7 @@ export class MapSegment {
     }
 
     await Assets.setPreferences({
-      crossOrigin: 'anonymous'
+      crossOrigin: '*'
     });
 
     await this.initializePixiApp(pixiContainer);
@@ -42,28 +50,76 @@ export class MapSegment {
     await this.AddMapParentContainer();
   }
 
-  private async initializePixiApp(container: HTMLElement) {
-    await this.pixiApp.init({ background: '#ffaadd', resizeTo: container });
-    container.appendChild(this.pixiApp.canvas);
+  // #region Snack Bar Queue
+
+  /** Appends a new message to the end of the snack bar queue.
+   * 
+   * @param imageUrl - Used to build the snack bar message
+   */
+  private queueImageLoadFailedSnackBar(imageUrl: string) {
+    var message = `Image \"${imageUrl}\" failed to load`;
+    this.snackBarMessageQueue.push(message);
+
+    if (this.currentSnackBar === undefined)
+      this.showNextSnackBarInQueue();
   }
 
+  /** If there is a message present at the front of the snack bar queue, presents that message as a new snack bar. */
+  private showNextSnackBarInQueue() {
+    this.currentSnackBar = undefined;
+
+    var nextMessage = this.snackBarMessageQueue.shift() ?? "";
+    if (nextMessage === "") return;
+
+    this.showSnackBar(nextMessage, 5000);
+  }
+
+  /** Opens an Angular Material snackbar in the upper right of the screen for a specifed duration. 
+   * 
+   * @param message - The text displayed on the snackbar
+   * @param duration - The number of milliseconds the snackbar will remain visible
+  */
+  private showSnackBar(message: string, duration: number) {
+    this.currentSnackBar = this.snackBar.open(message, undefined, {
+      duration: duration,
+      horizontalPosition: "right",
+      verticalPosition: "top"
+    });
+
+    this.currentSnackBar.afterDismissed().subscribe(() => {
+      this.showNextSnackBarInQueue();
+    });
+  }
+
+  // #endregion Snack Bar Queue
+
+  /** Initializes `this.pixiApp` and appends its resulting canvas as a child of the `appContainer` element. 
+   * @param appContainer - The HTML element that will contain the Pixi.JS canvas
+  */
+  private async initializePixiApp(appContainer: HTMLElement) {
+    await this.pixiApp.init({ backgroundAlpha: 0, resizeTo: appContainer });
+    appContainer.appendChild(this.pixiApp.canvas);
+  }
+
+  /** Creates a container, appends it to the `this.pixiApp` stage, and fills it with a centered map segment image. */
   private async AddMapParentContainer() {
     const map = new Container();
     this.pixiApp.stage.addChild(map);
 
-    const mapImage = await this.loadExternalAsset(this.segment().imageURL);
+    const mapImage = await this.loadExternalAsset(this.segment().imageURL)
+      .catch((error) => {
+        this.queueImageLoadFailedSnackBar(this.segment().imageURL); 
+      });
+
+    if(mapImage === undefined)
+      return;
     const mapSprite = new Sprite(mapImage);
     map.addChild(mapSprite);
-
-    // Move the map to the center of the screen
-    map.x = 0; //this.pixiApp.screen.width / 2;
-    map.y = 0; //this.pixiApp.screen.height / 2;
   }
 
-  private async loadExternalAsset(assetUrl: string) {
-    return await Assets.load({
-      src: assetUrl,
-      parser: 'texture'
+  private async loadExternalAsset(assetUrl: string) : Promise<Texture> {
+    return Assets.load<Texture>({
+      src: assetUrl
     });
   }
 }
