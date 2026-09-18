dbed4e3046ce8ba04f0df822db8f68be3db0295c
Iron Pegasus <kayleebanana@gmail.com>
2026-09-17 23:13:45 -0500

    Map analysis tweaks

--- src/app/components/sidenavs/map-analysis-options-sidenav/map-analysis-options-sidenav.ts
+++ src/app/components/sidenavs/map-analysis-options-sidenav/map-analysis-options-sidenav.ts
@@ -4,10 +4,11 @@ import { MatSelectModule } from '@angular/material/select';
 import { MapAnalysisEventService } from '../../../services/map-analysis-event-service';
 import { MapAnalysisDataService } from '../../../services/map-analysis-data-service';
 import { TileTerrainType } from '../../tile-terrain-type/tile-terrain-type';
+import { MatDivider } from '@angular/material/divider';
 
 @Component({
   selector: 'map-analysis-options-sidenav',
-  imports: [MatInputModule, MatSelectModule, TileTerrainType],
+  imports: [MatInputModule, MatSelectModule, TileTerrainType, MatDivider],
   templateUrl: './map-analysis-options-sidenav.html',
   styleUrl: './map-analysis-options-sidenav.scss',
 })
